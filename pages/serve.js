61442882914e505dec80435c61345936663195fc
psmetal <paulojam14@hotmail.com>
2026-03-05 19:49:50 -0400

    feat: atualizar importações para utilizar módulos ES6 e ajustar tipo no package.json

--- pages/serve.js
+++ pages/serve.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PrismaClient } from './generated/prisma/index.js';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const prisma = new PrismaClient();
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-require("dotenv/config");
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const { PrismaClient } = require("@prisma/client");
+import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const prisma = new PrismaClient();
