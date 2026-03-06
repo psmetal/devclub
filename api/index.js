4ff3f5acb59f2af62445aae6cb36862a33866121
psmetal <paulojam14@hotmail.com>
2026-03-05 22:06:25 -0400

    Fix serverless deployment

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json({ message: 'API disponível' });
+  res.status(200).json({ status: 'OK', message: 'API running' });
 }
+
